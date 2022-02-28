879ad06a0f2e723745ce158c8030041a7a14f6ee
Rho <rhodaphillipsosei@gmail.com>
2022-02-27 21:50:56 -0500

    Implement selectors

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,2 +1,19 @@
+export function getAppointmentsForDay(state, day) {
+ 
+  if(!state.days){
+    return [];
+  }
+  let dayFilter = state.days.filter(days => days.name === day)[0];
+  if(!dayFilter){
+    return [];
+  }
+  let result = [];
+  for(const id of dayFilter.appointments){
+    const apptState = state.appointments[id];
+    result.push(apptState);
+  }
+
+  return result;
+}
 
 
